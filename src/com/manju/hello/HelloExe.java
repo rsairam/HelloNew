92d60ec3c4ba639d13ac45ad58890a65940da5ef
Sairam <sreddi2006@gmail.com>
2014-04-27 19:42:16 -0700

    Added 3 programs
    Sample programs

---
+++ src/com/manju/hello/HelloExe.java
@@ -0,0 +1,13 @@
+package com.manju.hello;
+
+public class HelloExe {
+
+	/**
+	 * @param args
+	 */
+	public void execute(){
+		System.out.println("Helloooooooo Sairam");
+	}
+
+}
+	
---
+++ src/com/manju/hello/HelloExeTest.java
@@ -0,0 +1,30 @@
+package com.manju.hello;
+
+import org.junit.Test;
+
+public class HelloExeTest {
+	
+	@Test
+	public void test() {
+		System.out.println("Hello This is execution of junit tests");
+	}
+	
+	@Test
+	public void test1() {
+		System.out.println("Hello This is execution of junit tests-1");
+		
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("Hello This is execution of junit tests-2");
+		
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("Hello This is execution of junit tests-3");
+		
+	}
+
+}
